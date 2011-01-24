f5a5823c223fde98665d212cb0f22828928d244d
Mariusz Gronczewski <xani666@gmail.com>
2011-01-24 12:00:15 +0100

    Working prototype

--- lvautoresize.pl
+++ lvautoresize.pl
@@ -16,14 +16,15 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 use strict;
-use warnings;
-use Carp qw(cluck);
+#use warnings;
+#use diagnostic;
+use Carp qw(cluck croak);
 use Data::Dumper;
 use Sys::Syslog;
 use Config::General;
 my $cfgfile="/etc/lvauto.conf";
-my $dry_run = 1; # dont actually do anything
-my $verbose = 1; # be verbose. turn off if u wanna use it from cron
+my $dry_run = 0; # dont actually do anything
+my $verbose = 0; # be verbose. turn off if u wanna use it from cron
 
 # TODO add syslog logging to log functions
 # openlog('lvautoresize', 'cons,perror,nofatal', 'LOG_USER') or die "Can't open syslog";
@@ -42,21 +43,29 @@ while( my($vgname, $vg) = each (%$vglist)) {
 	my $fs = &get_fs_info($lv_path);
 	my $min_free = &get_bytes($lvconfig->{'min_free'});
 	my $min_free_percent = $lvconfig->{'min_free_percent'} / 100;
+	# TODO add optional "resize max(min_free,step_size)"
 	my $step_size = &get_bytes($lvconfig->{'step_size'});
 	if(defined($fs)) {
 	    # sanity check
-	    if($fs->{'total'} <= 0) {croak "Somehow total filesystem capacity returned by df is 0 or below, failing\n"}
+	    if($fs->{'total'} <= 0) {
+		croak "Somehow total filesystem capacity returned by df is 0 or below, failing\n";
+	    }
 	    if($fs->{'free'} < $min_free) {
 		&info("Volume $lv_path smaller than min_free, resizing\n");
 		&resize($lv_path, $fs->{'type'}, $step_size);
 	    }
 	    elsif ( ( $fs->{'free'} / $fs->{'total'} ) < $min_free_percent ) {
-		&info("Volume % of free space smalller than min_free_percent, resizing\n"
-		&resize($lv_path, $fs->{'type'}, $step_size);
+		&info("Volume $lv_path % of free space smalller than min_free_percent, resizing\n");
+		      &resize($lv_path, $fs->{'type'}, $step_size);
+	    } else {
+		info("Filesystem on $lv_path does not need to be resized\n");
+		# not in need of resize
+	    }
 	}
 	else {
 	    &warning("Filesystem on VG $vgname LV $lvname is not mounted, ignoring\n");
 	}
+
     }
 }
 
@@ -78,9 +87,39 @@ sub dump_config() {
 
 sub resize() {
     my $lv_path = shift;
-    my $fs_type;
-    my $step_size;
-    print "Dummy resize of $lv_path by $step_size with fs $fs_type\n";
+    my $fs_type = shift;
+    my $step_size = shift;
+    if (defined($config->{'filesystem'}{$fs_type}{'resize_cmd'} . "\n")) {
+	my $resize_cmd = $config->{'filesystem'}{$fs_type}{'resize_cmd'};
+	# TODO check if lv_size > partition_size (plus some MBs) then don't do resize
+	# so when previous run only resized LV but not partition on it (because of error)
+	# we won't resize it at infinitum
+	my $lvresize_txt = `/sbin/lvresize -L +$step_size $lv_path 2>&1`;
+	my $lvresize_exit_code = int( $? / 256 );
+	if($lvresize_exit_code == 0) {
+	    info($lvresize_txt);
+	    my $fsresize_txt = `$resize_cmd $lv_path 2>&1`;
+	    my $fsresize_exit_code = int( $? / 256 );
+	    if ($fsresize_exit_code == 0) {
+		info($fsresize_txt);
+		info("Resizing of $lv_path suceeded\n");
+	    }
+	    else {
+		error($fsresize_txt);
+		error("Resize of filesystem on $lv_path failed!\n");
+		return 1
+	    }
+	}
+	else {
+	    error($lvresize_txt);
+	    error("Resize of LV $lv_path failed!\n");
+	    return 1
+	}
+    }
+    else {
+	error("resize_cmd for fs $fs_type not defined!\n");
+	return 1
+    }
     return;
 }
 
